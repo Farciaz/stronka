25f08832f30616c34b681b55caa877dcc75f36df
Kacper Dziedzic <kacper.dziedzic@teb.edu.pl>
2024-05-20 10:00:07 +0200

    ddddd

--- login.php
+++ login.php
@@ -1,20 +1,6 @@
 <?php
-//zaimportuj kod klasy
-require_once('class/User.class.php');
-//inicjalizacja sesji
+require_once('class/Profile.class.php');
 session_start();
-
-
-if (isset($_REQUEST['email']) && isset($_REQUEST['password'])) {
-    //wysłano formularz - przechwyć i obrób dane
-    $email = $_REQUEST['email'];
-    $password = $_REQUEST['password'];
-    //wywołujemy metodę klasy
-    //metody statyczne w PHP wywołuje się poprzez ::
-    $result = User::Login($email, $password);
-    //$result zawiera true jeśli udało się zalogować lub 
-    //fasle w innym wypadku
-}
 ?>
 <!DOCTYPE html>
 <html lang="pl">
@@ -22,46 +8,21 @@ if (isset($_REQUEST['email']) && isset($_REQUEST['password'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formularz logowania</title>
+    <title>Strona główna</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <div id="container">
-        <h1 class="text-center mt-5 mb-5">Zaloguj się:</h1>
-        <div id="loginForm" class="col-4 offset-4 mt-5">
-            <form action="login.php" method="post">
-                <label for="emailInput" class="form-label">Adres e-mail:</label>
-                <input type="email" class="form-control mb-3" name="email" id="emailInput">
+<?php
+if(isset($_REQUEST['profileID']))
+    $profileID = $_REQUEST['profileID'];
+else
+    $profileID = 2; //domyślny profil
 
-                <label for="passwordInput" class="form-label">Hasło:</label>
-                <input type="password" class="form-control mb-3" name="password" id="passwordInput">
+$p = Profile::Get($profileID);
 
-                <button type="submit" class="btn btn-primary w-100 mt-3">Zaloguj</button>
-                
-            </form>
-                <a href="index.php">
-                <button class="btn btn-primary w-100 mt-3">Powrót</button>
-                </a>
-            <?php
-            if (isset($result)) {
-                if ($result) {
-                    echo "Użytkownik zalogowany<br>";
-                    echo '<a href="index.php">Przejdź do głównej strony</a>';
-                    
-                } else {
-                    echo "Użytkownik nie zalogowany";
-                }
-            }
-
-            ?>
-        </div>
-    </div>
-    <?php
-    echo "<pre>";
-    var_dump($_SESSION);
-    ?>
+var_dump($p);
+?>
 </body>
-
 </html>
